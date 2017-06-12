ac83cff399a00ca4274d76f3080e0e9831d0e238
DabinderSingh <dibs_singh@hotmail.com>
2017-06-12 22:53:11 +0100

    file update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Application entry 
  * Example - run a particular store or website:
  * --------------------------------------------
- * require __DIR__ . '/app/bootstrap.php';
+ * 
  * $params = $_SERVER;
  * $params[\Magento\Store\Model\StoreManager::PARAM_RUN_CODE] = 'website2';
  * $params[\Magento\Store\Model\StoreManager::PARAM_RUN_TYPE] = 'website';
